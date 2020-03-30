42d918b945a0c7cdbafc605e0600e160579bbe20
Leonardo <leonardocapone89@gmail.com>
2020-03-30 12:12:35 +0200

    release 1 implementation

---
+++ src/game.js
@@ -0,0 +1,27 @@
+class Game {
+
+    players = []
+    rounds = 0
+
+    constructor(players) {
+        if(players.length < 2 || players.length > 8) 
+            throw new TypeError("players must be between 2 and 8");
+        this.players = players.sort(() => Math.random() - 0.5);
+        this.rounds = 0
+    }
+
+    play(turns) {     
+        Array(turns).fill().forEach(() => this.play_turn());
+    }
+
+    play_turn() {
+        this.rounds += 1
+        players.forEach(player => player.play_turn());
+    }
+
+    shuffle(array) {
+        array.sort(() => Math.random() - 0.5);
+    }
+}
+
+module.exports = Game;
---
+++ tests/game.test.js
@@ -0,0 +1,72 @@
+const expect = require('chai').expect;
+
+const Game = require('../src/game');
+const Player = require('../src/player');
+
+describe('Game Tests', () => {
+        it('Create a game with two players named Horse and Car', () => {
+            players = []
+            players.push(new Player('Horse'))
+            players.push(new Player('Car'))
+            game = new Game(players)
+            expect(game.class).to.equals(Game.class)
+        })
+
+        it('Try to create a game with < 2 players. When attempting to play the game, it will fail', () => {
+            players = []
+            players.push(new Player('Horse'))
+            expect(() => new Game(players)).to.throw()
+        })
+
+        it('Try to create a game with > 8 players. When attempting to play the game, it will fail', () => {
+            players = []
+            Array(10).fill().forEach((_, i) => players.push(new Player("Horse_#"+i)))
+            expect(() => new Game(players)).to.throw()
+        })
+
+        it('Create a game with two players named Horse and Car. Within creating 100 games, both orders [Horse, Car] and [Car, Horse] occur', () => {
+            horse = new Player('Horse')
+            car = new Player('Car')
+            players = [horse, car]
+            
+            horse_car_case = false
+            car_horse_case = false
+            
+            Array(100).fill().forEach(() => {     
+                game = new Game(players)
+                horse_car_case |= JSON.stringify(game.players)==JSON.stringify([horse, car])
+                car_horse_case |= JSON.stringify(game.players)==JSON.stringify([car, horse])    
+            });        
+            
+            expect(Boolean(horse_car_case)).to.be.true
+            expect(Boolean(car_horse_case)).to.be.true
+        })
+
+        it('Create a game and play, verify that the total rounds was 20 and that each player played 20 rounds' , () => {
+            players = [
+                new Player('Horse'),
+                new Player('Car')
+            ]
+
+            game = new Game(players)
+            game.play(20)
+            expect(game.rounds).to.equal(20)
+
+            players.forEach((player) => expect(player.rounds).to.equal(20));                
+        })
+
+        it('Create a game and play, verify that in every round the order of the players remained the same' , () => {
+            players = [
+                new Player('Horse'),
+                new Player('Car')
+            ]
+            
+            game = new Game(players)
+            game_players = game.players
+            
+            Array(20).fill().forEach(() => {
+                game.play(1)
+                expect(game.players).to.equals(game_players)
+            })
+        })
+});
